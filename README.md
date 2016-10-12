015c99dc93dfa58ab8d1ec4fa70c3396baaf4bd7
David Jessup <david.jessup@sydney.edu.au>
2016-10-12 13:37:51 +1100

    Project scaffold

---
+++ README.md
@@ -0,0 +1,11 @@
+# Home Automation Simulator
+A browser-based, simulated home automation system.
+
+## Requirements
+TBC
+
+## Building
+TBC
+
+## Acknowledgments
+TBC
---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+import $ from 'jquery';
+import bootstrap from 'bootstrap/js/bootstrap.js';
+import 'bootstrap/css/bootstrap.min.css!';
+import 'app/css/dashboard.css!'
