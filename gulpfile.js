84abbe485b9786eb2aa170d9a0770c9280d91b28
David Jessup <david.jessup@sydney.edu.au>
2016-10-19 01:52:50 +1100

    Removed broken tests

--- gulpfile.js
+++ gulpfile.js
@@ -31,18 +31,11 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
-// Run test once and exit
+// Run tests (currently broken)
 gulp.task('test', function (done) {
     new karmaServer({
         configFile: __dirname + '/karma.conf.js',
         singleRun: true
-    }, done).start();
-});
-
-// Watch for file changes and re-run tests on each change
-gulp.task('tdd', function (done) {
-    new karmaServer({
-        configFile: __dirname + '/karma.conf.js'
     }, done).start();
 });
 
